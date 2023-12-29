5d0681ca643fb9e69b73779d5a221ac708ee94cf
Renato <renamedrj@gmail.com>
2023-12-28 22:51:19 -0300

    Mapper factory

--- UnitTests/Mapper/MapperFactory.cs
+++ UnitTests/Mapper/MapperFactory.cs
@@ -10,4 +10,22 @@ public static class MapperFactory
         var config = new MapperConfiguration(cfg => cfg.AddProfile<CategoriesMapper>());
         return config.CreateMapper();
     }
+
+    public static IMapper GetEnumMapper()
+    {
+        var config = new MapperConfiguration(cfg => cfg.AddProfile<EnumMapper>());
+        return config.CreateMapper();
+    }
+
+    public static IMapper GetPeriodsMapper()
+    {
+        var config = new MapperConfiguration(cfg => cfg.AddProfile<PeriodMapper>());
+        return config.CreateMapper();
+    }
+
+    public static IMapper GetTransactionsMapper()
+    {
+        var config = new MapperConfiguration(cfg => cfg.AddProfile<TransactionsMapper>());
+        return config.CreateMapper();
+    }
 }
